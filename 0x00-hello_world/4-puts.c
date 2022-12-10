81d521fe5fbe4dcb256285d8e774d04a083db088
KIPRUTO-200 <kipruabel@gmail.com>
2022-12-10 13:40:00 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<string.h>
 /*
  * main - print c programing with puts character
  *
@@ -7,5 +8,5 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-       return (0);
-}              
+	return (0);
+}
