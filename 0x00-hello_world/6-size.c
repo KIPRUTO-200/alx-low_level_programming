117679e95da851d11cb300d818daac4764a52ca2
KIPRUTO-200 <kipruabel@gmail.com>
2022-12-11 07:09:24 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	printf("size of a char: %d byte(s)\n",sizeof(char));
-	printf("size of an int: %d byte(s)/n",sizeof(int));
-	pritnf("size of long int %d byte(s)n\,sizeof(long int));
-	printf("size of long long %d byte(s),\n",sizeof(long long int));
-	printf("size of flat: %d byte(s)\n",sizeof(flot));
-	return (0);
+	printf("size of a char: %d byte(s)\n", sizeof(char));
+	printf("size of an int: %d byte(s)\n", sizeof(int));
+	pritnf("size of a long int: %lu byte(s)\n", sizeof(longint));
+	printf("size of long long int: %lu byte(s)\n" sizeof(long long int));
+	printf("size of float: %lu byte(s)\n" sizeof(flout));
+	return (a);
 }
